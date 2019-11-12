0808473982f53a45e2451526b2c1f4d08e65826a
ema920 <yuki1994ema@gmail.com>
2019-11-12 14:17:42 +0900

    修正点

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -36,14 +36,14 @@ $(function () {
             processData: false,
             contentType: false,
         })
-       　 .done(function(data){
-          　var html = buildHTML(data);
-          　$('.messages').append(html);
-          　$('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');         
-         　 $('form')[0].reset();
+         .done(function(data){
+           var html = buildHTML(data);
+           $('.messages').append(html);
+           $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');         
+           $('form')[0].reset();
         })
-        　.fail(function(){
-          alert('error');
+          .fail(function(){
+            alert('error');
         });
         return false;
    });
