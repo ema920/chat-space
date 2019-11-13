998035c81401b05f10eb4cf715c42ca95bc37ef0
ema920 <yuki1994ema@gmail.com>
2019-11-13 19:04:56 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,5 @@
 $(function () {
+  // $(document).on('turbolinks:load', function(){
 
   function buildHTML(message) {
 
@@ -24,53 +25,58 @@ $(function () {
     return html;
   };
 
-   $('#new_message').on('submit', function(e){
-        e.preventDefault()
-        var formData = new FormData(this);
-        var url = $(this).attr('action')
-        $.ajax({
-            url: url,
-            type: "POST",
-            data: formData,
-            dataType: 'json',
-            processData: false,
-            contentType: false,
-        })
-         .done(function(data){
-           var html = buildHTML(data);
-           $('.messages').append(html);
-           $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');         
-           $('form')[0].reset();
-        })
-          .fail(function(){
-            alert('error');
-        });
-        return false;
-   });
+  
+     $('#new_message').on('submit', function(e){
+          e.preventDefault()
+          var formData = new FormData(this);
+          var url = $(this).attr('action')
+          $.ajax({
+              url: url,
+              type: "POST",
+              data: formData,
+              dataType: 'json',
+              processData: false,
+              contentType: false,
+          })
+           .done(function(data){
+             console.log(data)
+             var html = buildHTML(data);
+             $('.messages').append(html);
+             $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');         
+             $('form')[0].reset();
+          })
+            .fail(function(){
+              alert('error');
+          });
+          return false;
+     });
 
-    
-  var reloadMessages = function () {
-    if (window.location.href.match(/\/groups\/\d+\/messages/)){
-      var last_message_id = $('.message:last').data("message-id"); 
-      // var group_id = $(".group").data("group-id");
+      
+    // var reloadMessages = function () {
+    //   if (window.location.href.match(/\/groups\/\d+\/messages/)){
+    //     var last_message_id = $('.message:last').data("message-id"); 
+    //     // var group_id = $(".group").data("group-id");
 
-      $.ajax({ 
-        url: "api/messages", 
-        type: 'get',
-        dataType: 'json', 
-        data: {last_id: last_message_id} 
-      })
-      .done(function (messages) { 
-        var insertHTML = '';
-        messages.forEach(function (message) {
-          insertHTML = buildHTML(message); 
-          $('.messages').append(insertHTML);
-        });
-      })
-      .fail(function () {
-        alert('自動更新に失敗しました');
-      });
-    };
-  };
-  setInterval(reloadMessages, 5000);
+    //     $.ajax({ 
+    //       url: "api/messages", 
+    //       type: 'get',
+    //       dataType: 'json', 
+    //       data: {last_id: last_message_id} 
+    //     })
+    //     .done(function (messages) { 
+    //       console.log(messages)
+    //       var insertHTML = '';
+    //       messages.forEach(function (message) {
+    //         insertHTML = buildHTML(message); 
+    //         $('.messages').append(insertHTML);
+    //         $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+    //       });
+    //     })
+    //     .fail(function () {
+    //       alert('自動更新に失敗しました');
+    //     });
+    //   };
+    // };
+    // setInterval(reloadMessages, 5000);
+  //  });
 });
